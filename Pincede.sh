597b13ce225105e2643d3671f255b62f99a7b0fb
Deep Saha <deepsaha288@yahoo.com>
2020-08-20 15:46:44 +0530

    added alphabet in to pattern

--- Pincede.sh
+++ Pincede.sh
@@ -4,10 +4,10 @@ shopt -s extglob
 echo "enter the pincode "
 read pincode
 
-pat="^[0-9]{6}$"
+pat="^[A-Z]{0}[0-9]{6}"
 if [[ $pincode =~ $pat ]]
 then
         echo "This is Valid pincode"
 else
-        echo "This is Invalid pincode"
+        echo " This is Invalid pincode"
 fi
